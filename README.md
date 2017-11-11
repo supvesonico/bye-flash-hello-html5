37291567ddfba129ff85ed8c19852235ff14c252
levinit <levinit@outlook.com>
2017-11-12 00:04:37 +0800

    修复芒果tv cntv cctv 和网易公开课　添加QQ音乐MV

--- README.md
+++ README.md
@@ -5,24 +5,24 @@ bye-flash-hello-html5 | 再见flash 你好html5
 # 介绍
 
 - 这是什么？
-  使用HTML5播放视频的greasymonkey script（油猴脚本）。
+  使用HTML5播放视频的××monkey script。
 - 名字什么意思？
   滚吧flash，html5快到碗里来。
 
 
 ## 支持网站
 
+（可以直接使用的意思就是不使用任何插件／扩展／脚本就能够播放视频。）
+
 - ~~[优酷](http://youku.com)~~
 
-  优酷可以直接播放HTML5啦，先从脚本中去掉试试看。
+  优酷可以直接播放HTML5啦，划！
 
 - [爱奇艺](http://iqiyi.com)
 
   提示：使用较新版本的Edge15+、Chrome52+的用户**应该**可以直接使用HTML5播放。
 
-- [央视网](http://cctv.com)
-
-  提示：央视直播(live)无法观看。
+- [央视网](http://cctv.com)（cctv.com和cntv.cn）
 
 - [网易公开课](http://open.163.com)
 
@@ -42,8 +42,17 @@ bye-flash-hello-html5 | 再见flash 你好html5
 
   提示：大部分浏览器可以直接使用HTML5播放。
 
+- [QQ音乐MV](https://y.qq.com/portal/mv_lib.html)
+
+  提示：Chrome52+可以直接使用HTML5播放。
+
+  ​
+
 # 使用
 1. 根据你的浏览器情况安装[脚本管理器](https://greasyfork.org/zh-CN)
+
+   注意：**Firefox用户暂时不要使用greasymonkey扩展**，建议使用[Tampermonkey](https://tampermonkey.net/)，油猴新版本插件大有问题，许多脚本都会失效（2017.11留）。
+
 2. 安装此脚本，一下两种方式任选其一：
    - 查看[bye-flash-hello-html5.user.js](https://github.com/levinit/bye-flash-hello-html5/blob/master/bye-flash-hello-html5.user.js) ，然后点击[Raw](https://github.com/levinit/bye-flash-hello-html5/raw/master/bye-flash-hello-html5.user.js)按钮；
    - 到Greasy Fork网站的[bye-flash-hello-html5 | 再见flash 你好html5](https://greasyfork.org/zh-CN/scripts/30879-bye-flash-hello-html5-%E5%86%8D%E8%A7%81flash-%E4%BD%A0%E5%A5%BDhtml5) 信息页，点击[安装脚本](https://greasyfork.org/scripts/30879-bye-flash-hello-html5-%E5%86%8D%E8%A7%81flash-%E4%BD%A0%E5%A5%BDhtml5/code/bye-flash-hello-html5%20%7C%20%E5%86%8D%E8%A7%81flash%20%E4%BD%A0%E5%A5%BDhtml5.user.js)按钮。
@@ -52,8 +61,8 @@ bye-flash-hello-html5 | 再见flash 你好html5
 
 某些代码是借（fu）鉴（zhi）的，比如：
 
-- [优酷原生HTML5播放器](https://greasyfork.org/nl/scripts/30414-%E4%BC%98%E9%85%B7%E5%8E%9F%E7%94%9Fhtml5%E6%92%AD%E6%94%BE%E5%99%A8)　简单有效的一句代码
+- ~~[优酷原生HTML5播放器](https://greasyfork.org/nl/scripts/30414-%E4%BC%98%E9%85%B7%E5%8E%9F%E7%94%9Fhtml5%E6%92%AD%E6%94%BE%E5%99%A8)　简单有效的一句代码~~
 
 ---
 
-试看如今的网络，已是html5的世界，用flash，电脑要！爆！炸！好！吗！
+试看如今的网络，已是html5的世界，用flash，电脑要！爆！炸！好！**吗！**
--- bye-flash-hello-html5.user.js
+++ bye-flash-hello-html5.user.js
@@ -7,100 +7,128 @@
 // @include     *://*.iqiyi.com/*html*
 // @include     *://v.qq.com/*
 // @include     *://y.qq.com/*
-// @include     *://*.cctv.com/*
+// @include     *://*.cctv.com/*html
+// @include     *://*.cntv.cn/video/*
 // @include     *://www.mgtv.com/*/*.html*
+// @include     *://m.mgtv.com/*
 // @include     *://zt.mgtv.com/act/*
 // @include     *://www.mgtv.com/news
 // @include     *://www.mgtv.com/live
 // @include     *://live.mgtv.com*
 // @include     *://*.icourse163.org/*
 // @include     *://open.163.com/movie*
+// @include     *://m.open.163.com/movie*
 // @include     *://study.163.com/course/courseLearn*
 // @include     *://mooc.study.163.com/learn/*
 // @include     *://live.bilibili.com/*
 // @run-at      document-start
-// @version     1.7.1
+// @version     1.7.4
 // @grant       none
 // ==/UserScript==
 //'use strict';
-var ua = null;  //user-agent取值
-var isPhone = false;  //是否使用移动ua
-var ele = function (element) {  //获取元素对象的函数
-    return document.querySelector(element);
+var ua = null; //user-agent
+var isPhone = false; //是否使用移动ua
+//获取元素对象的函数
+var ele = function(element) {
+  return document.querySelector(element);
 };
-var changeUA = function (ua) { //更改ua的函数
-    Object.defineProperty(navigator, 'userAgent', {
-        value: ua,
-        writable: false,
-        configurable: false,
-        enumerable: true
-    });
+//更改ua的函数
+var changeUA = function(ua) {
+  Object.defineProperty(navigator, "userAgent", {
+    value: ua,
+    writable: false,
+    configurable: false,
+    enumerable: true
+  });
 };
+//判断移动ua相关信息
+(function() {
+  //这些网站使用移动ua
+  var phone = ["cctv", ".163", "mgtv", "iqiyi"];
 
-(function () {  //判断移动ua相关信息
-    var phone = [  //这些网站使用移动ua
-        'cctv',
-        '.163',
-        'mgtv',
-        'iqiyi'
-    ];
-
-    if (location.host.indexOf('iqiyi') >= 0 && navigator.userAgent.indexOf('Firefox') === -1) {
-        isPhone = false;  //爱奇艺的html5不支持firefox
-    } else {
-        for (var i = 0; i < phone.length; i++) {
-            if (location.host.indexOf(phone[i]) >= 0) {
-                isPhone = true;
-            }
-        }
+  if (
+    location.host.indexOf("iqiyi") >= 0 &&
+    navigator.userAgent.indexOf("Firefox") === -1
+  ) {
+    isPhone = false; //爱奇艺的html5不支持firefox（非firefox酱油路过）
+  } else {
+    for (var i = 0; i < phone.length; i++) {
+      if (location.host.indexOf(phone[i]) >= 0) {
+        isPhone = true;
+      }
     }
+  }
 })();
 
-if (isPhone) {  //移动ua的网站
-    ua = 'Mozilla/5.0 (Linux; U; Android 4.0.4; GT-I9300 Build/IMM76D) AppleWebKit/601.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/601.1.46';
+if (isPhone) {
+  //移动ua的网站
+  if (location.host.indexOf("mgtv") >= 0) {
+    ua =
+      "Mozilla/5.0 (iPad; U; CPU OS 4_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8F191 Safari/6533.18.5";
+  } else {
+    ua =
+      "Mozilla/5.0 (Linux; Android 7.0; PLUS Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.98 Mobile Safari/537.36";
+  }
+  changeUA(ua);
+} else {
+  //使用chrome、mac、safari等ua
+  if (
+    (location.host === "v.qq.com" ||
+      location.host === "y.qq.com" ||
+      location.host === "live.bilibil.com") &&
+    navigator.userAgent.indexOf("Edge") === -1
+  ) {
+    console.log("路过"); //Edge的腾讯视频、QQ音乐MV和b站直播不支持html5（不是Edge就打酱油路过吧）
+  } else if (location.href.indexOf("iqiyi") >= 0) {
+    console.log("路过"); //非Firefox的酱油路过（若未firefo，则已经在前面进入了isPhone===true分支啦）
+  } else if (location.host.indexOf("le.com") >= 0) {
+    //尊贵的le.com对mac+safari情有独钟……单独提出来吧
+    ua =
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8";
     changeUA(ua);
-} else {  //使用chrome、mac、safari等ua
-    if ((location.host === 'v.qq.com' || location.host === 'y.qq.com' || location.host === 'live.bilibil.com') && navigator.userAgent.indexOf('Edge') === -1) {
-        console.log('支持html5');  //Edge的腾讯视频、QQ音乐MV和b站直播不支持html5
-    } else if (location.href.indexOf('iqiyi') >= 0) {
-        console.log('支持html5');
-    } else if (location.host.indexOf('le.com') >= 0) {  //le.com对mac+safari情有独钟
-        ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8';
-        changeUA(ua);
-    } else {  //chrome ua
-        ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/603.3.8';
-        changeUA(ua);
-    }
+  } else {
+    //其余诸位通通进入披上这顶伪装衣吧
+    ua =
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/603.3.8";
+    changeUA(ua);
+  }
 }
 
-if (location.href.search('open.163') >= 0) {  //网易公开课
-    setTimeout(function () {
-        var v = ele('.video-wrapper video');
-        ele('body').style.backgroundColor = '#e4f0eb';
-        if (!!v === true) {  //设置视频播放区域的高度
-            v.setAttribute('style', 'height:100%');
-        }
-    }, 2333);
+if (location.href.search("open.163") >= 0) {
+  //网易公开课
+  setTimeout(function() {
+    var v = ele(".video-wrapper video");
+    ele("body").style.backgroundColor = "#e4f0eb";
+    if (!!v === true) {
+      //设置视频播放区域的高度
+      v.setAttribute("style", "height:100%");
+    }
+  }, 2333);
 }
 
-window.onload = function () {
-    if (isPhone === true && (location.href.search('study.163') >= 0 || location.href.search('iqiyi.com') >= 0)) {
-        var videoElement = ele('video');  //视频元素
-        if (!!videoElement === true) {
-            videoElement.setAttribute('controls', 'controls');  //显示播放控制条
-            if (location.href.search('iqiyi.com') >= 0) {
-                iqiyi();
-            }
-        }
+window.onload = function() {
+  if (
+    isPhone === true &&
+    (location.href.search("study.163") >= 0 ||
+      location.href.search("iqiyi.com") >= 0)
+  ) {
+    var videoElement = ele("video"); //视频元素
+    if (!!videoElement === true) {
+      videoElement.setAttribute("controls", "controls"); //显示播放控制条
+      if (location.href.search("iqiyi.com") >= 0) {
+        iqiyi();
+      }
     }
-    function iqiyi() {  //爱奇艺的相关调整
-        var defaultProcess = ele('.process-response');  //爱奇艺默认的进度条
-        var control2 = ele('.bottom');  //播放下一个视频和调整画质的控制条
-        if (!!control2 === true) {
-            control2.style.bottom = '25px';  //播放下一个视频和调整画质的控制条往上放
-            if (!!defaultProcess === true) {
-                defaultProcess.style.display = 'none';  //让默认进度条消失
-            }
-        }
+  }
+  function iqiyi() {
+    //爱奇艺的相关调整
+    var defaultProcess = ele(".process-response"); //爱奇艺默认的进度条
+    var control2 = ele(".bottom"); //播放下一个视频和调整画质的控制条
+    if (!!control2 === true) {
+      control2.style.bottom = "25px"; //播放下一个视频和调整画质的控制条往上放
+      if (!!defaultProcess === true) {
+        defaultProcess.style.display = "none"; //让默认进度条消失
+      }
     }
+  }
 };
